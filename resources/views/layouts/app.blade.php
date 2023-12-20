e578f9249b7bb1a2b81808b11fc5065e9d7322dd
joni.medina <jonathan.medina.91@gmail.com>
2023-12-20 03:18:39 -0300

    CRUD - Docentes & Estudiantes - OK

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+
 </head>
 
 <header class="p-3 text-bg-dark">
